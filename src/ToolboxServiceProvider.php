63f05a83ff1cb958ae8ba219126b25e9521a04e3
hitman <944594252@qq.com>
2017-09-01 11:46:37 +0800

    减少修改量

--- src/ToolboxServiceProvider.php
+++ src/ToolboxServiceProvider.php
@@ -15,9 +15,12 @@ class ToolboxServiceProvider extends ServiceProvider
 
         // 后台管理员生成
         $this->publishes([__DIR__ . "/config/auth.php" => config_path('auth.php')], 'toolbox');
-        $this->publishes([__DIR__ . "/Controllers/Admin" => app_path("Http/Controllers/Admin")], 'toolbox');
+        $this->publishes([__DIR__ . "/Controllers/Admin/Controller.php.template" => app_path("Http/Controllers/Admin/Controller.php")], 'toolbox');
+        $this->publishes([__DIR__ . "/Controllers/Admin/AdminController.php.template" => app_path("Http/Controllers/Admin/AdminController.php")], 'toolbox');
         $this->publishes([__DIR__ . "/routes/web.php" => base_path('routes/web.php')], 'toolbox');
-        $this->publishes([__DIR__ . "/Models/" => app_path('Models')], 'toolbox');
+        $this->publishes([__DIR__ . "/Traits/ModelTrait.php.template" => app_path('Traits/ModelTrait.php')], 'toolbox');
+        $this->publishes([__DIR__ . "/Models/Model.php.template" => app_path('Models/Model.php')], 'toolbox');
+        $this->publishes([__DIR__ . "/Models/Admin.php.template" => app_path('Models/Admin.php')], 'toolbox');
         $this->publishes([__DIR__ . "/databases/migrations/2017_08_24_080433_create_table_admins.php" => database_path('migrations/2017_08_24_080433_create_table_admins.php')], 'toolbox');
     }
     
