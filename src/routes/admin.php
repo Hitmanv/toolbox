759265d3068f680b1c44da09ad72ff30ac523671
hitman <944594252@qq.com>
2018-12-06 10:23:21 +0800

    后台中间件修改

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -4,7 +4,7 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function(){
     Route::get('login', 'AdminController@getLogin');
     Route::post('login', 'AdminController@postLogin');
 
-    Route::group(['middleware' => ['auth:admin']], function(){
+    Route::group(['middleware' => ['admin_auth']], function(){
         Route::get('/', function(){
             return response()->json(\Illuminate\Support\Facades\Auth::guard('admin')->user());
         });
